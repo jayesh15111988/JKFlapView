f0ec9f392ff940005ab65e3c8172d573c956ca49
Jayesh Kawli <j.kawli@gmail.com>
2015-08-30 15:56:04 -0400

    Added animation effect around z-axis

--- JK3DFlapView/ViewController.h
+++ JK3DFlapView/ViewController.h
@@ -8,17 +8,38 @@
 
 #import <UIKit/UIKit.h>
 
+typedef NS_ENUM(NSUInteger, FlapOpening3DMode) {
+    FlapOpeningMode3DTop,
+    FlapOpeningMode3DLeft,
+    FlapOpeningMode3DBottom,
+    FlapOpeningMode3DRight,
+    
+};
+
+typedef NS_ENUM(NSUInteger, FlapOpening2DDirection) {
+    FlapOpening2DDirectionClockwise,
+    FlapOpening2DDirectionAnticlockwise
+};
+
 typedef NS_ENUM(NSUInteger, FlapOpeningMode) {
-    FlapOpeningModeTop,
-    FlapOpeningModeLeft,
-    FlapOpeningModeBottom,
-    FlapOpeningModeright
+    FlapOpeningMode2D,
+    FlapOpeningMode3D
+};
+
+typedef NS_ENUM(NSUInteger, FlapOpening2DPosition) {
+    FlapOpening2DPositionTopLeft,
+    FlapOpening2DPositionBottomLeft,
+    FlapOpening2DPositionBottomRight,
+    FlapOpening2DPositionTopRight
 };
 
 
 @interface ViewController : UIViewController
 
-@property (nonatomic, assign) FlapOpeningMode flapOpeningMode;
+@property (nonatomic, assign) FlapOpening3DMode flapOpening3DModeValue;
+@property (nonatomic, assign) FlapOpening2DDirection flapOpening2DDirectionValue;
+@property (nonatomic, assign) FlapOpeningMode flapOpeningModeValue;
+@property (nonatomic, assign) FlapOpening2DPosition flapOpening2DPositionValue;
 
 @end
 
--- JK3DFlapView/ViewController.m
+++ JK3DFlapView/ViewController.m
@@ -14,6 +14,7 @@
 @property (nonatomic, strong) CALayer *doorLayer;
 @property (nonatomic, assign) BOOL flapOpened;
 @property (nonatomic, strong) NSString *animatingPropertyKeyPath;
+@property (nonatomic, assign) CGFloat flapOpeningAngle;
 
 @end
 
@@ -22,7 +23,11 @@
 - (void)viewDidLoad {
 	[super viewDidLoad];
 
-	self.flapOpeningMode = FlapOpeningModeTop;
+	self.flapOpeningAngle = 90.0;
+	self.flapOpening3DModeValue = FlapOpeningMode3DBottom;
+	self.flapOpeningModeValue = FlapOpeningMode2D;
+	self.flapOpening2DPositionValue = FlapOpening2DPositionBottomRight;
+	self.flapOpening2DDirectionValue = FlapOpening2DDirectionAnticlockwise;
 	CATextLayer *vibrantLabel = [CATextLayer layer];
 	vibrantLabel.string = @"fs df dsf dsf ds fds fas das d asd ";
 	vibrantLabel.frame = CGRectMake (0, 32, 128, 64);
@@ -36,11 +41,15 @@
 	parentView.backgroundColor = [UIColor greenColor];
 	parentView.layer.contents = (__bridge id)[UIImage imageNamed:@"sp.jpg"].CGImage;
 
+	CALayer *doorParentLayer = [CALayer layer];
+	doorParentLayer.frame = CGRectMake (0, 0, 128, 128);
+
 	self.doorLayer = [CALayer layer];
 	self.doorLayer.frame = CGRectMake (0, 0, 128, 128);
 	self.doorLayer.backgroundColor = [UIColor clearColor].CGColor;
 	self.doorLayer.contents = (__bridge id)[UIImage imageNamed:@"blur.jpg"].CGImage;
 	self.doorLayer.opacity = 0.9;
+	[doorParentLayer addSublayer:self.doorLayer];
 
 	[self setupPositionAndAnchorPoint];
 	[self.doorLayer addSublayer:vibrantLabel];
@@ -58,22 +67,39 @@
 - (void)setupPositionAndAnchorPoint {
 	CGPoint flappingLayerPosition;
 	CGPoint flappingLayerAnchorPoint;
-	if (self.flapOpeningMode == FlapOpeningModeTop) {
-		flappingLayerPosition = CGPointMake (64, 0);
-		flappingLayerAnchorPoint = CGPointMake (0.5, 0);
-		self.animatingPropertyKeyPath = @"transform.rotation.x";
-	} else if (self.flapOpeningMode == FlapOpeningModeBottom) {
-		flappingLayerPosition = CGPointMake (64, 128);
-		flappingLayerAnchorPoint = CGPointMake (0.5, 1);
-		self.animatingPropertyKeyPath = @"transform.rotation.x";
-	} else if (self.flapOpeningMode == FlapOpeningModeLeft) {
-		flappingLayerPosition = CGPointMake (0, 64);
-		flappingLayerAnchorPoint = CGPointMake (0, 0.5);
-		self.animatingPropertyKeyPath = @"transform.rotation.y";
-	} else if (self.flapOpeningMode == FlapOpeningModeright) {
-		flappingLayerPosition = CGPointMake (128, 64);
-		flappingLayerAnchorPoint = CGPointMake (1, 0.5);
-		self.animatingPropertyKeyPath = @"transform.rotation.y";
+	if (self.flapOpeningModeValue == FlapOpeningMode3D) {
+		if (self.flapOpening3DModeValue == FlapOpeningMode3DTop) {
+			flappingLayerPosition = CGPointMake (64, 0);
+			flappingLayerAnchorPoint = CGPointMake (0.5, 0);
+			self.animatingPropertyKeyPath = @"transform.rotation.x";
+		} else if (self.flapOpening3DModeValue == FlapOpeningMode3DLeft) {
+			flappingLayerPosition = CGPointMake (0, 64);
+			flappingLayerAnchorPoint = CGPointMake (0, 0.5);
+			self.animatingPropertyKeyPath = @"transform.rotation.y";
+		} else if (self.flapOpening3DModeValue == FlapOpeningMode3DBottom) {
+			flappingLayerPosition = CGPointMake (64, 128);
+			flappingLayerAnchorPoint = CGPointMake (0.5, 1);
+			self.animatingPropertyKeyPath = @"transform.rotation.x";
+		} else if (self.flapOpening3DModeValue == FlapOpeningMode3DRight) {
+			flappingLayerPosition = CGPointMake (128, 64);
+			flappingLayerAnchorPoint = CGPointMake (1, 0.5);
+			self.animatingPropertyKeyPath = @"transform.rotation.y";
+		}
+	} else {
+		if (self.flapOpening2DPositionValue == FlapOpening2DPositionTopLeft) {
+			flappingLayerPosition = CGPointMake (0, 0);
+			flappingLayerAnchorPoint = CGPointMake (0, 0);
+		} else if (self.flapOpening2DPositionValue == FlapOpening2DPositionBottomLeft) {
+			flappingLayerPosition = CGPointMake (0, 128);
+			flappingLayerAnchorPoint = CGPointMake (0, 1);
+		} else if (self.flapOpening2DPositionValue == FlapOpening2DPositionBottomRight) {
+			flappingLayerPosition = CGPointMake (128, 128);
+			flappingLayerAnchorPoint = CGPointMake (1, 1);
+		} else if (self.flapOpening2DPositionValue == FlapOpening2DPositionTopRight) {
+			flappingLayerPosition = CGPointMake (1, 0);
+			flappingLayerAnchorPoint = CGPointMake (128, 0);
+		}
+		self.animatingPropertyKeyPath = @"transform.rotation.z";
 	}
 	self.doorLayer.position = flappingLayerPosition;
 	self.doorLayer.anchorPoint = flappingLayerAnchorPoint;
@@ -88,17 +114,32 @@
 	CGFloat toVal;
 	CGColorRef toColorValue;
 	CGColorRef fromColorValue;
+	CATransform3D transformToApply;
 
 	if (!self.flapOpened) {
 		fromVal = 0;
-		toVal = degreesToRadians (110);
+		toVal = degreesToRadians (_flapOpeningAngle);
 		toColorValue = [UIColor redColor].CGColor;
 		fromColorValue = [UIColor blackColor].CGColor;
 	} else {
-		fromVal = degreesToRadians (110);
+		fromVal = degreesToRadians (_flapOpeningAngle);
 		toVal = 0;
 		toColorValue = [UIColor blackColor].CGColor;
 		fromColorValue = [UIColor redColor].CGColor;
+	}
+
+	if (self.flapOpeningModeValue == FlapOpeningMode2D) {
+		if (self.flapOpening2DDirectionValue == FlapOpening2DDirectionAnticlockwise) {
+			toVal = -1 * toVal;
+		}
+		transformToApply = CATransform3DMakeRotation (toVal, 0, 0, 1);
+	} else if (self.flapOpeningModeValue == FlapOpeningMode3D) {
+		if (self.flapOpening3DModeValue == FlapOpeningMode3DTop ||
+		    self.flapOpening3DModeValue == FlapOpeningMode3DBottom) {
+			transformToApply = CATransform3DMakeRotation (toVal, 1, 0, 0);
+		} else {
+			transformToApply = CATransform3DMakeRotation (toVal, 0, 1, 0);
+		}
 	}
 
 	colorChangeAnimation.toValue = (__bridge id)toColorValue;
@@ -114,11 +155,10 @@
 	animationGroup.delegate = self;
 	animationGroup.fillMode = kCAFillModeBoth;
 	[self.doorLayer addAnimation:animationGroup forKey:@"rotationAnimation"];
-	self.doorLayer.transform = CATransform3DMakeRotation (toVal, 1, 0, 0);
+	self.doorLayer.transform = transformToApply;
 }
 
 - (void)animationDidStop:(CAAnimation *)anim finished:(BOOL)flag {
-	NSLog (@"Stopped");
 }
 
 static inline CGFloat degreesToRadians (CGFloat degrees) { return degrees * (M_PI / 180.0); }
